724d7d5d6fe0b6ea31b57f5bce8980a7c2f378a4
WingedGlider <william.armstrong967@gmail.com>
2023-11-01 06:04:18 -0400

    change divs to buttons, remove all alert text, update gameplay loop code

--- script.js
+++ script.js
@@ -1,45 +1,64 @@
 let userScore = 0;
 let compScore = 0;
 let playerChoice = null;
-playGame();
+const choices = document.createElement('h3');
+const winText = document.querySelector('.win-text');
+const shell = document.querySelector('body');
+const score = document.querySelector('.score');
+const buttons = document.querySelectorAll('.rock, .paper, .scissors');
+const start = document.querySelector('.start');
+start.addEventListener('click', () => beginGame());
 
-function evalPlayerChoice(choice){/*Unnecessary function, text input no longer requested*/
-    if ( !( choice == "scissors" || choice == "rock" || choice == "paper")){
-    choice = prompt("Not a valid input... Please enter: Rock, Paper, or Scissors?");
-    choice = evalPlayerChoice(choice);
-    }
-    return choice;
+function beginGame(){
+    userScore = 0;
+    compScore = 0;
+    playerChoice = null;
+    shell.removeChild(start);
+    shell.appendChild(winText);
+    score.textContent = (userScore + " - " + compScore);
+    winText.textContent = ("Press a button to begin!");
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            playerChoice = button.className;
+            checkOutcome(getComputerChoice(),playerChoice);
+        });
+    });
 }
 
-function getComputerChoice(){/*this is fine*/
+function getComputerChoice(){
     let choice = Math.floor(Math.random()*3);
     if (choice == 0) return "rock";
     if (choice == 1) return "paper";
     else return "scissors";
 }
 
-function checkOutcome(comp, user){/*place alerts in dom objects*/ 
-    if (comp == user) alert("Neither wins, it's a draw!");
+function checkOutcome(comp, user){
+    if (comp == user) {
+    winText.textContent = ("Neither wins, it's a draw!");
+    shell.appendChild(choices);
+    shell.removeChild(choices);
+    }
     else if ((user == "rock" && comp == "scissors") || (user == "scissors" && comp == "paper") || (user == "paper" && comp == "rock")){
-        alert ("You win, " +user+ " beats " +comp+ "!"); 
+        winText.textContent = ("You win, " +user+ " beats " +comp+ "!"); 
         userScore++;
     }else{
-        alert ("You lose, " +user+ " loses to " +comp+ "!");
+        winText.textContent = ("You lose, " +user+ " loses to " +comp+ "!");
         compScore++;
     }
-    alert ("The score is "+userScore+" to " +compScore+ "!");
-}
-
-function playGame(){/*remove code that produces alerts and switch to dom objects, input should be created with buttons and converted to text for comparisons*/ 
-        prompt("Hello, user! Rock, Paper, or Scissors?").toLowerCase();
-        playerChoice = prompt("Round " +i+ ", Rock, Paper, or Scissors?").toLowerCase();
-        playerChoice = evalPlayerChoice(playerChoice);
-        alert("You chose " +playerChoice.toLowerCase()+"!");
-        let compChoice = getComputerChoice();
-        alert("Your opponent chose " +compChoice.toLowerCase()+"!");
-        checkOutcome(compChoice,playerChoice);
-        alert("Game over, check the console for results!");
-        if (compScore == userScore) console.log("Game ended with a tie.");
-        else if (compScore < userScore) console.log ("Game ended in favor of player, " +userScore+ " to " +compScore+".");
-        else console.log("Game ended in favor of computer, " +userScore+ " to " +compScore+".");
+    choices.textContent = ("You chose " +user+", computer chose " +comp+"!");
+    shell.appendChild(choices);
+    score.textContent = (userScore + " - " + compScore);
+    if (userScore == 5 || compScore == 5){
+        shell.removeChild(choices);
+        winText.textContent = ('Refresh to play again!')
+        if (userScore > compScore){
+            score.innerHTML = ("You win!<br>"+ userScore + " - " + compScore);
+        }else{
+            score.innerHTML = ("You lose!<br>"+ userScore + " - " + compScore);
+        }
+        buttons.forEach(button => {
+            button.disabled = true;
+        });
     }
+
+};
